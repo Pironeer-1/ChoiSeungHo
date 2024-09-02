542cb0efcd93a1f92d227f85c110011e23b8a74a
chltmdgh522 <chltmdgh517@naver.com>
2024-09-03 01:16:39 +0900

    1-2 빠르게 시작하기 보강

--- pironeer1/src/App.js
+++ pironeer1/src/App.js
@@ -16,6 +16,58 @@
 // }
 
 
+// import { useState } from 'react';
+//
+// export default function MyApp() {
+//     return (
+//         <div>
+//             <h1>Counters that update separately</h1>
+//             <MyButton />
+//             <MyButton />
+//         </div>
+//     );
+// }
+//
+// function MyButton() {
+//     const [count, setCount] = useState(0);
+//
+//     function handleClick() {
+//         setCount(count + 1);
+//     }
+//
+//     return (
+//         <button onClick={handleClick}>
+//             Clicked {count} times
+//         </button>
+//     );
+// }
+
+// import { useState } from 'react';
+//
+// export default function MyApp() {
+//     const [count, setCount] = useState(0);
+//
+//     function handleClick() {
+//         setCount(count + 1);
+//     }
+//
+//     return (
+//         <div>
+//             <h1>Counters that update together</h1>
+//             <MyButton count={count} onClick={handleClick} />
+//             <MyButton count={count} onClick={handleClick} />
+//         </div>
+//     );
+// }
+//
+// function MyButton({ count, onClick }) {
+//     return (
+//         <button onClick={onClick}>
+//             Clicked {count} times
+//         </button>
+//     );
+// }
+
 // export default function Square() {
 //     return (
 //         <>
@@ -478,6 +530,7 @@
 // }
 
 
+// 리액트 시작하기 실시간 검색!! 신기하네 =====================
 import {useState} from 'react';
 
 function FilterableProductTable({products}) {
