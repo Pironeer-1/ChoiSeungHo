6557028eb70eec797efd3bf7e7d81e5ac2ef341d
chltmdgh522 <chltmdgh517@naver.com>
2024-09-04 12:49:44 +0900

    3-3 JSX를 사용한 마크업

--- prioneer1-3/src/App.js
+++ prioneer1-3/src/App.js
@@ -1,19 +1,34 @@
-function Profile() {
+// function Profile() {
+//     return (
+//         <img
+//             src="https://i.imgur.com/MK3eW3As.jpg"
+//             alt="Katherine Johnson"
+//         />
+//     );
+// }
+//
+// export default function Gallery() {
+//     return (
+//         <section>
+//             <h1>Amazing scientists</h1>
+//             <Profile />
+//             <Profile />
+//             <Profile />
+//         </section>
+//     );
+// }
+
+export default function Bio() {
     return (
-        <img
-            src="https://i.imgur.com/MK3eW3As.jpg"
-            alt="Katherine Johnson"
-        />
-    );
+        <div class="intro">
+            <h1>Welcome to my website!</h1>
+        </div>
+    <p class="summary">
+        You can find my thoughts here.
+        <br><br>
+            <b>And <i>pictures</b></i> of scientists!
+    </p>
+);
 }
 
-export default function Gallery() {
-    return (
-        <section>
-            <h1>Amazing scientists</h1>
-            <Profile />
-            <Profile />
-            <Profile />
-        </section>
-    );
-}
+
