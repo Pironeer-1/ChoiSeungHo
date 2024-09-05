749dd5f237d13de874c318593496e62721ef45ac
chltmdgh522 <chltmdgh517@naver.com>
2024-09-05 14:15:07 +0900

    4-2 상태(State): 컴포넌트의 기억 공간

--- pironeer1-4/src/App.js
+++ pironeer1-4/src/App.js
@@ -135,14 +135,88 @@
 //         </div>
 //     );
 // }
-export default function Signup() {
+// export default function Signup() {
+//     return (
+//         <form onSubmit={e => {
+//             e.preventDefault();
+//             alert('Submitting!');
+//         }}>
+//             <input />
+//             <button>Send</button>
+//         </form>
+//     );
+// }
+
+
+//
+// export default function Gallery() {
+//     const [index, setIndex] = useState(0);
+//
+//     function handleClick() {
+//         setIndex(index + 1);
+//     }
+//
+//     let sculpture = sculptureList[index];
+//     return (
+//         <>
+//             <button onClick={handleClick}>
+//                 Next
+//             </button>
+//             <h2>
+//                 <i>{sculpture.name} </i>
+//                 by {sculpture.artist}
+//             </h2>
+//             <h3>
+//                 ({index + 1} of {sculptureList.length})
+//             </h3>
+//             <img
+//                 src={sculpture.url}
+//                 alt={sculpture.alt}
+//             />
+//             <p>
+//                 {sculpture.description}
+//             </p>
+//         </>
+//     );
+// }
+
+
+import { useState } from 'react';
+import { sculptureList } from './data.js';
+
+export default function Gallery() {
+    const [index, setIndex] = useState(0);
+    const [showMore, setShowMore] = useState(false);
+
+    function handleNextClick() {
+        setIndex(index + 1);
+    }
+
+    function handleMoreClick() {
+        setShowMore(!showMore);
+    }
+
+    let sculpture = sculptureList[index];
     return (
-        <form onSubmit={e => {
-            e.preventDefault();
-            alert('Submitting!');
-        }}>
-            <input />
-            <button>Send</button>
-        </form>
+        <>
+            <button onClick={handleNextClick}>
+                Next
+            </button>
+            <h2>
+                <i>{sculpture.name} </i>
+                by {sculpture.artist}
+            </h2>
+            <h3>
+                ({index + 1} of {sculptureList.length})
+            </h3>
+            <button onClick={handleMoreClick}>
+                {showMore ? 'Hide' : 'Show'} details
+            </button>
+            {showMore && <p>{sculpture.description}</p>}
+            <img
+                src={sculpture.url}
+                alt={sculpture.alt}
+            />
+        </>
     );
 }
