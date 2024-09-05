579d4d5750c40af587c09eca5b09dbcf6f894d3d
chltmdgh522 <chltmdgh517@naver.com>
2024-09-05 14:06:24 +0900

    4-1 이벤트에 응답하기

--- pironeer1-4/src/App.js
+++ pironeer1-4/src/App.js
@@ -1,25 +1,148 @@
-import logo from './logo.svg';
-import './App.css';
+// export default function Button() {
+//     function handleClick() {
+//         alert('You clicked me!');
+//     }
+//
+//     return (
+//         <button onClick={handleClick}>
+//             Click me
+//         </button>
+//     );
+// }
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+// function AlertButton({ message, children }) {
+//     return (
+//         <button onClick={() => alert(message)}>
+//             {children}
+//         </button>
+//     );
+// }
+//
+// export default function Toolbar() {
+//     return (
+//         <div>
+//             <AlertButton message="Playing!">
+//                 Play Movie
+//             </AlertButton>
+//             <AlertButton message="Uploading!">
+//                 Upload Image
+//             </AlertButton>
+//         </div>
+//     );
+// }
+//
+// function Button({ onClick, children }) {
+//     return (
+//         <button onClick={onClick}>
+//             {children}
+//         </button>
+//     );
+// }
+//
+// function PlayButton({ movieName }) {
+//     function handlePlayClick() {
+//         alert(`Playing ${movieName}!`);
+//     }
+//
+//     return (
+//         <Button onClick={handlePlayClick}>
+//             Play "{movieName}"
+//         </Button>
+//     );
+// }
+//
+// function UploadButton() {
+//     return (
+//         <Button onClick={() => alert('Uploading!')}>
+//             Upload Image
+//         </Button>
+//     );
+// }
+//
+// export default function Toolbar() {
+//     return (
+//         <div>
+//             <PlayButton movieName="Kiki's Delivery Service" />
+//             <UploadButton />
+//         </div>
+//     );
+// }
+
+
+// function Button({ onSmash, children }) {
+//     return (
+//         <button onClick={onSmash}>
+//             {children}
+//         </button>
+//     );
+// }
+// // children은 컴포넌트의 하위 요소들을 전달받는 역할!
+// export default function App() {
+//     return (
+//         <div>
+//             <Button onSmash={() => alert('Playing!')}>
+//                 Play Movie
+//             </Button>
+//             <Button onSmash={() => alert('Uploading!')}>
+//                 Upload Image
+//             </Button>
+//         </div>
+//     );
+// }
+
+
+// export default function App() {
+//     return (
+//         <Toolbar
+//             onPlayMovie={() => alert('Playing!')}
+//             onUploadImage={() => alert('Uploading!')}
+//         />
+//     );
+// }
+
+// function Toolbar({ onPlayMovie, onUploadImage }) {
+//     return (
+//         <div>
+//             <Button onClick={onPlayMovie}>
+//                 Play Movie
+//             </Button>
+//             <Button onClick={onUploadImage}>
+//                 Upload Image
+//             </Button>
+//         </div>
+//     );
+// }
+//
+// function Button({ onClick, children }) {
+//     return (
+//         <button onClick={onClick}>
+//             {children}
+//         </button>
+//     );
+// }
+
+// export default function Toolbar() {
+//     return (
+//         <div className="Toolbar" onClick={() => {
+//             alert('You clicked on the toolbar!');
+//         }}>
+//             <button onClick={() => alert('Playing!')}>
+//                 Play Movie
+//             </button>
+//             <button onClick={() => alert('Uploading!')}>
+//                 Upload Image
+//             </button>
+//         </div>
+//     );
+// }
+export default function Signup() {
+    return (
+        <form onSubmit={e => {
+            e.preventDefault();
+            alert('Submitting!');
+        }}>
+            <input />
+            <button>Send</button>
+        </form>
+    );
 }
-
-export default App;
