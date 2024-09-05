84c3c313b1baafce1e076b02db2fb12a2b85eeb4
chltmdgh522 <chltmdgh517@naver.com>
2024-09-06 00:46:36 +0900

    4-7 상태에서 배열 업데이트하기

--- pironeer1-4/src/App.js
+++ pironeer1-4/src/App.js
@@ -702,85 +702,258 @@
 //         </>
 //     );
 // }
+//
+//
+// import { useImmer } from 'use-immer';
+//
+// export default function Form() {
+//     const [person, updatePerson] = useImmer({
+//         name: 'Niki de Saint Phalle',
+//         artwork: {
+//             title: 'Blue Nana',
+//             city: 'Hamburg',
+//             image: 'https://i.imgur.com/Sd1AgUOm.jpg',
+//         }
+//     });
+//
+//     function handleNameChange(e) {
+//         updatePerson(draft => {
+//             draft.name = e.target.value;
+//         });
+//     }
+//
+//     function handleTitleChange(e) {
+//         updatePerson(draft => {
+//             draft.artwork.title = e.target.value;
+//         });
+//     }
+//
+//     function handleCityChange(e) {
+//         updatePerson(draft => {
+//             draft.artwork.city = e.target.value;
+//         });
+//     }
+//
+//     function handleImageChange(e) {
+//         updatePerson(draft => {
+//             draft.artwork.image = e.target.value;
+//         });
+//     }
+//
+//     return (
+//         <>
+//             <label>
+//                 Name:
+//                 <input
+//                     value={person.name}
+//                     onChange={handleNameChange}
+//                 />
+//             </label>
+//             <label>
+//                 Title:
+//                 <input
+//                     value={person.artwork.title}
+//                     onChange={handleTitleChange}
+//                 />
+//             </label>
+//             <label>
+//                 City:
+//                 <input
+//                     value={person.artwork.city}
+//                     onChange={handleCityChange}
+//                 />
+//             </label>
+//             <label>
+//                 Image:
+//                 <input
+//                     value={person.artwork.image}
+//                     onChange={handleImageChange}
+//                 />
+//             </label>
+//             <p>
+//                 <i>{person.artwork.title}</i>
+//                 {' by '}
+//                 {person.name}
+//                 <br />
+//                 (located in {person.artwork.city})
+//             </p>
+//             <img
+//                 src={person.artwork.image}
+//                 alt={person.artwork.title}
+//             />
+//         </>
+//     );
+// }
+
+// import { useState } from 'react';
+//
+// let nextId = 0;
+//
+// export default function List() {
+//     const [name, setName] = useState('');
+//     const [artists, setArtists] = useState([]);
+//
+//     return (
+//         <>
+//             <h1>Inspiring sculptors:</h1>
+//             <input
+//                 value={name}
+//                 onChange={e => setName(e.target.value)}
+//             />
+//             <button onClick={() => {
+//                 artists.push({
+//                     id: nextId++,
+//                     name: name,
+//                 });
+//             }}>Add</button>
+//             <ul>
+//                 {artists.map(artist => (
+//                     <li key={artist.id}>{artist.name}</li>
+//                 ))}
+//             </ul>
+//         </>
+//     );
+// }
 
 
-import { useImmer } from 'use-immer';
+// import { useState } from 'react';
+//
+// let initialArtists = [
+//     { id: 0, name: 'Marta Colvin Andrade' },
+//     { id: 1, name: 'Lamidi Olonade Fakeye'},
+//     { id: 2, name: 'Louise Nevelson'},
+// ];
+//
+// export default function List() {
+//     const [artists, setArtists] = useState(
+//         initialArtists
+//     );
+//
+//     return (
+//         <>
+//             <h1>Inspiring sculptors:</h1>
+//             <ul>
+//                 {artists.map(artist => (
+//                     <li key={artist.id}>
+//                         {artist.name}{' '}
+//                         <button onClick={() => {
+//                             setArtists(
+//                                 artists.filter(a =>
+//                                     a.id !== artist.id
+//                                 )
+//                             );
+//                         }}>
+//                             Delete
+//                         </button>
+//                     </li>
+//                 ))}
+//             </ul>
+//         </>
+//     );
+// }
 
-export default function Form() {
-    const [person, updatePerson] = useImmer({
-        name: 'Niki de Saint Phalle',
-        artwork: {
-            title: 'Blue Nana',
-            city: 'Hamburg',
-            image: 'https://i.imgur.com/Sd1AgUOm.jpg',
-        }
-    });
 
-    function handleNameChange(e) {
-        updatePerson(draft => {
-            draft.name = e.target.value;
-        });
-    }
+// import { useState } from 'react';
+//
+// let nextId = 3;
+// const initialList = [
+//     { id: 0, title: 'Big Bellies', seen: false },
+//     { id: 1, title: 'Lunar Landscape', seen: false },
+//     { id: 2, title: 'Terracotta Army', seen: true },
+// ];
+//
+// export default function BucketList() {
+//     const [myList, setMyList] = useState(initialList);
+//     const [yourList, setYourList] = useState(
+//         initialList
+//     );
+//
+//     function handleToggleMyList(artworkId, nextSeen) {
+//         const myNextList = [...myList];
+//         const artwork = myNextList.find(
+//             a => a.id === artworkId
+//         );
+//         artwork.seen = nextSeen;
+//         setMyList(myNextList);
+//     }
+//
+//     function handleToggleYourList(artworkId, nextSeen) {
+//         const yourNextList = [...yourList];
+//         const artwork = yourNextList.find(
+//             a => a.id === artworkId
+//         );
+//         artwork.seen = nextSeen;
+//         setYourList(yourNextList);
+//     }
+//
+//     return (
+//         <>
+//             <h1>Art Bucket List</h1>
+//             <h2>My list of art to see:</h2>
+//             <ItemList
+//                 artworks={myList}
+//                 onToggle={handleToggleMyList} />
+//             <h2>Your list of art to see:</h2>
+//             <ItemList
+//                 artworks={yourList}
+//                 onToggle={handleToggleYourList} />
+//         </>
+//     );
+// }
+//
+// function ItemList({ artworks, onToggle }) {
+//     return (
+//         <ul>
+//             {artworks.map(artwork => (
+//                 <li key={artwork.id}>
+//                     <label>
+//                         <input
+//                             type="checkbox"
+//                             checked={artwork.seen}
+//                             onChange={e => {
+//                                 onToggle(
+//                                     artwork.id,
+//                                     e.target.checked
+//                                 );
+//                             }}
+//                         />
+//                         {artwork.title}
+//                     </label>
+//                 </li>
+//             ))}
+//         </ul>
+//     );
+// }
 
-    function handleTitleChange(e) {
-        updatePerson(draft => {
-            draft.artwork.title = e.target.value;
-        });
-    }
 
-    function handleCityChange(e) {
-        updatePerson(draft => {
-            draft.artwork.city = e.target.value;
-        });
-    }
+import { useState } from 'react';
 
-    function handleImageChange(e) {
-        updatePerson(draft => {
-            draft.artwork.image = e.target.value;
-        });
+let nextId = 3;
+const initialList = [
+    { id: 0, title: 'Big Bellies' },
+    { id: 1, title: 'Lunar Landscape' },
+    { id: 2, title: 'Terracotta Army' },
+];
+
+export default function List() {
+    const [list, setList] = useState(initialList);
+
+    function handleClick() {
+        const nextList = [...list];
+        nextList.reverse();
+        setList(nextList);
     }
 
     return (
         <>
-            <label>
-                Name:
-                <input
-                    value={person.name}
-                    onChange={handleNameChange}
-                />
-            </label>
-            <label>
-                Title:
-                <input
-                    value={person.artwork.title}
-                    onChange={handleTitleChange}
-                />
-            </label>
-            <label>
-                City:
-                <input
-                    value={person.artwork.city}
-                    onChange={handleCityChange}
-                />
-            </label>
-            <label>
-                Image:
-                <input
-                    value={person.artwork.image}
-                    onChange={handleImageChange}
-                />
-            </label>
-            <p>
-                <i>{person.artwork.title}</i>
-                {' by '}
-                {person.name}
-                <br />
-                (located in {person.artwork.city})
-            </p>
-            <img
-                src={person.artwork.image}
-                alt={person.artwork.title}
-            />
+            <button onClick={handleClick}>
+                Reverse
+            </button>
+            <ul>
+                {list.map(artwork => (
+                    <li key={artwork.id}>{artwork.title}</li>
+                ))}
+            </ul>
         </>
     );
 }
