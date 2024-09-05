be5b5262505ef03961494965aaa6edfba39417c6
chltmdgh522 <chltmdgh517@naver.com>
2024-09-05 14:35:10 +0900

    4-4. 상태를 스냅샷으로 사용하기

--- pironeer1-4/src/App.js
+++ pironeer1-4/src/App.js
@@ -223,23 +223,137 @@
 // export default function App() {
 //     return <h1>Hello World</h1>
 // }
-import { useState, useEffect } from 'react';
-import Clock from './Clock.js';
+// import { useState, useEffect } from 'react';
+// import Clock from './Clock.js';
+//
+// function useTime() {
+//     const [time, setTime] = useState(() => new Date());
+//     useEffect(() => {
+//         const id = setInterval(() => {
+//             setTime(new Date());
+//         }, 1000);
+//         return () => clearInterval(id);
+//     }, []);
+//     return time;
+// }
+//
+// export default function App() {
+//     const time = useTime();
+//     return (
+//         <Clock time={time.toLocaleTimeString()} />
+//     );
+// }
+// import { useState } from 'react';
+//
+// export default function Form() {
+//     const [isSent, setIsSent] = useState(false);
+//     const [message, setMessage] = useState('Hi!');
+//     if (isSent) {
+//         return <h1>Your message is on its way!</h1>
+//     }
+//     return (
+//         <form onSubmit={(e) => {
+//             e.preventDefault();
+//             setIsSent(true);
+//             sendMessage(message);
+//         }}>
+//       <textarea
+//           placeholder="Message"
+//           value={message}
+//           onChange={e => setMessage(e.target.value)}
+//       />
+//             <button type="submit">Send</button>
+//         </form>
+//     );
+// }
+//
+// function sendMessage(message) {
+//     // ...
+// }
 
-function useTime() {
-    const [time, setTime] = useState(() => new Date());
-    useEffect(() => {
-        const id = setInterval(() => {
-            setTime(new Date());
-        }, 1000);
-        return () => clearInterval(id);
-    }, []);
-    return time;
-}
 
-export default function App() {
-    const time = useTime();
+// import { useState } from 'react';
+//
+// export default function Counter() {
+//     const [number, setNumber] = useState(0);
+//
+//     return (
+//         <>
+//             <h1>{number}</h1>
+//             <button onClick={() => {
+//                 setNumber(number + 1);
+//                 setNumber(number + 1);
+//                 setNumber(number + 1);
+//             }}>+3</button>
+//         </>
+//     )
+// }
+
+
+// import { useState } from 'react';
+//
+// export default function Counter() {
+//     const [number, setNumber] = useState(0);
+//
+//     return (
+//         <>
+//             <h1>{number}</h1>
+//             <button onClick={() => {
+//                 setNumber(number + 5);
+//                 alert(number);
+//             }}>+5</button>
+//         </>
+//     )
+// }
+//
+// import { useState } from 'react';
+//
+// export default function Counter() {
+//     const [number, setNumber] = useState(0);
+//
+//     return (
+//         <>
+//             <h1>{number}</h1>
+//             <button onClick={() => {
+//                 setNumber(number + 5);
+//                 setTimeout(() => {
+//                     alert(number);
+//                 }, 3000);
+//             }}>+5</button>
+//         </>
+//     )
+// }
+
+import { useState } from 'react';
+
+export default function Form() {
+    const [to, setTo] = useState('Alice');
+    const [message, setMessage] = useState('Hello');
+
+    function handleSubmit(e) {
+        e.preventDefault();
+        setTimeout(() => {
+            alert(`You said ${message} to ${to}`);
+        }, 5000);
+    }
+
     return (
-        <Clock time={time.toLocaleTimeString()} />
+        <form onSubmit={handleSubmit}>
+            <label>
+                To:{' '}
+                <select
+                    value={to}
+                    onChange={e => setTo(e.target.value)}>
+                    <option value="Alice">Alice</option>
+                    <option value="Bob">Bob</option>
+                </select>
+            </label>
+            <textarea
+                placeholder="Message"
+                value={message}
+                onChange={e => setMessage(e.target.value)}
+            />
+            <button type="submit">Send</button>
+        </form>
     );
 }
