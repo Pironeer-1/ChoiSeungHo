a072a006a18189e424548e7eb768d194acd9ed74
chltmdgh522 <chltmdgh517@naver.com>
2024-09-05 14:27:02 +0900

    4-3 렌더링과 커밋

--- pironeer1-4/src/index.js
+++ pironeer1-4/src/index.js
@@ -1,17 +1,17 @@
-import React from 'react';
+import React, {StrictMode} from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
+import Gallery from './Gallery.js';
+
+// const root = ReactDOM.createRoot(document.getElementById('root'));
+// root.render(<Gallery />);
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
+// root.render(<Gallery />);
 root.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>
+    <StrictMode>
+        <App />
+    </StrictMode>
 );
-
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
